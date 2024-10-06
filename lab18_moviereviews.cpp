336bfc7a0325d655ccf906d0596c4a09050ec21a
winstonjose01 <winston.jose@gmail.com>
2024-10-05 22:30:49 -0700

    revision-9: adding more input validation

--- lab18_moviereviews.cpp
+++ lab18_moviereviews.cpp
@@ -28,20 +28,26 @@ int main(){
     Node *head = nullptr;   // Initialize the head of linked list
     int choice;
 
-    cout << "Which linked list method should we use? \n";
-    cout << "\t[1] New nodes are added at the head of the linked list\n";
-    cout << "\t[2] New nodes are added at the tail of the linked list\n";
-    cout << "Choice :";
-    cin >> choice;
-    cin.ignore();
-
-    switch(choice){
-        case 1: add_node_front(head);
-            break;
-        case 2: add_node_tail(head);
-            break;
-
-    }
+    while(true)
+        cout << "Which linked list method should we use? \n";
+        cout << "\t[1] New nodes are added at the head of the linked list\n";
+        cout << "\t[2] New nodes are added at the tail of the linked list\n";
+        cout << "Choice :";
+        cin >> choice;
+        cin.ignore();
+        if (cin.fail || choice< 1 || choice > 2){
+            cin.clear()
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input, select [1] or [2] only"
+        }
+        else{
+            switch(choice){
+                case 1: add_node_front(head);
+                    break;
+                case 2: add_node_tail(head);
+                    break;
+            }
+        }
     
     return 0;
 
